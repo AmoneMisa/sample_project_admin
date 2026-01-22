424b1b144c271266ced7cc64b60be1ef6bacbe68
WhitesLove <kubai.rita5@gmail.com>
2026-01-22 20:14:05 +0300

    Added testimonials

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routers import languages, translations
+from .routers import languages, translations, testimonials
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -11,6 +11,8 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://127.0.0.1:3001",
         "http://localhost:3001",
+        "http://127.0.0.1:3002",
+        "http://localhost:3002",
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -19,3 +21,4 @@ app.add_middleware(
 
 app.include_router(languages.router)
 app.include_router(translations.router)
+app.include_router(testimonials.router)
