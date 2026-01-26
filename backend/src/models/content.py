f05aa36727d1cf9d8d9126deb92b5fb2dba11d93
WhitesLove <kubai.rita5@gmail.com>
2026-01-26 12:23:06 +0300

    Rework git repo

--- backend/src/models/content.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, JSON
-from database import Base
-
-class Content(Base):
-    __tablename__ = "content"
-
-    id = Column(Integer, primary_key=True, index=True)
-    key = Column(String, unique=True, index=True)
-    value = Column(JSON)
--- backend/src/schemas/content.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Any
-
-class ContentSchema(BaseModel):
-    key: str
-    value: Any
-
-    class Config:
-        orm_mode = True
